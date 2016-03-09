5706f55d2e27e5dbdeedafcbb8cc1e568c9d4d12
naelrouz <nael_rouz@mail.ru>
2016-03-09 15:58:34 +0300

    ed

--- init.sh
+++ init.sh
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-=======
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
->>>>>>> f567c6a52cb2de889032b6ffe30287aabcd3c989
