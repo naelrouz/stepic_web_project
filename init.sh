5057a10713fbec2b2742359bbb33c5a8f91acc11
naelrouz <nael_rouz@mail.ru>
2016-03-09 16:34:01 +0300

    10

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #sudo rm /etc/nginx/sites-available/default
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo nginx restart
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
