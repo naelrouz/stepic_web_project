3039c1dc48f346ba3f6f1c0e2fb356dd70244589
naelrouz <nael_rouz@mail.ru>
2016-03-15 21:36:12 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-#sudo rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/etc/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
+#sudo /etc/init.d/nginx restart
+#sudo /etc/init.d/gunicorn restart
 #gunicorn -b 0.0.0.0:8080 hello &
